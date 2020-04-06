2647bd5627741d1b119b4ff082549244399c6183
rlisowski10 <ryan.lisowski@outlook.com>
2020-04-06 07:59:21 -0600

    Added basic single-row filter functionality for songs, albums, and artists.

--- start.py
+++ start.py
@@ -133,21 +133,75 @@ def deletePlaylist(playlist_id):
 
 @app.route('/playlist/<int:playlist_id>/new/searchartist', methods=['GET', 'POST'])
 def searchArtist(playlist_id):
+    songs = None
+    if request.method == 'POST':
+        # Get all user-provided values from the UI.
+        filterAttribute = request.form['attribute'].lower().replace(' ', '_')
+        filterOperator = request.form['operator']
+        userText = request.form['usrText']
+
+        # Gets the artist attribute based on the attribute string from the UI.
+        filterAttribute = getattr(Artist, filterAttribute)
+        if filterOperator == '>':
+            artists = session.query(Artist).filter(filterAttribute > userText).all()
+        elif filterOperator == '<':
+            artists = session.query(Artist).filter(filterAttribute < userText).all()
+        else:
+            artists = session.query(Artist).filter(filterAttribute == userText).all()
+
+        # Get all albums for the provided artists.
+        albums = []
+        for artist in artists:
+            album_results = session.query(Album).filter(Album.artist_id == artist.id).all()
+            for album in album_results:
+                albums.append(album)
+
+        # Get all songs for the provided albums.
+        songs = []
+        for album in albums:
+            song_results = session.query(Song).filter(Song.album_id == album.id).all()
+            for song in song_results:
+                songs.append(song)
+    
     playlists = session.query(Playlist).all()
     playlistName = session.query(Playlist).filter_by(id=playlist_id).one()
     return render_template(
         'searchArtist.html',
         title='Search Artist',
-        playlists=playlists, playlistName=playlistName)
+        playlists=playlists, playlistName=playlistName, songs=songs)
 
 @app.route('/playlist/<int:playlist_id>/new/searchalbum', methods=['GET', 'POST'])
 def searchAlbum(playlist_id):
+    songs = None
+    if request.method == 'POST':
+        # Get all user-provided values from the UI.
+        filterAttribute = request.form['attribute'].lower().replace(' ', '_')
+        filterOperator = request.form['operator']
+        userText = request.form['usrText']
+
+        # Gets the album attribute based on the attribute string from the UI.
+        filterAttribute = getattr(Album, filterAttribute)
+        if filterOperator == '>':
+            albums = session.query(Album).filter(filterAttribute > userText).all()
+        elif filterOperator == '<':
+            albums = session.query(Album).filter(filterAttribute < userText).all()
+        else:
+            albums = session.query(Album).filter(filterAttribute == userText).all()
+
+        # Get all songs for the provided albums.
+        songs = []
+        for album in albums:
+            song_results = session.query(Song).filter(Song.album_id == album.id).all()
+            for song in song_results:
+                songs.append(song)
+
     playlists = session.query(Playlist).all()
     playlistName = session.query(Playlist).filter_by(id=playlist_id).one()
+
     return render_template(
         'searchAlbum.html',
-        title='Search Album',
-        playlists=playlists, playlistName=playlistName)
+        title='Search Albums',
+        playlists=playlists, playlistName=playlistName, songs=songs)
 
 @app.route('/playlist/<int:playlist_id>/new/searchsong', methods=['GET', 'POST'])
 def searchSong(playlist_id):
